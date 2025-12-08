9f956daa03f65f99b451ffb6f765cab28aee5e27
Ian Jure Macalisang <ianjure.data@gmail.com>
2025-12-08 18:45:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -218,18 +218,7 @@ with col1:
 
 
         # CHART SECTION
-        title, date = st.columns(2)
-        with title:
-            st.write(f"#### **{st.session_state.selected_barangay} Dengue Forecast**")
-        with date:
-            st.markdown(
-                f"""
-                <div style='display: flex; height: 100%; align-items: flex-end; justify-content: flex-end;'>
-                    <h6 style='margin: 0;'>{date_range_str}</h6>
-                </div>
-                """,
-                unsafe_allow_html=True
-            )
+        st.write(f"#### **{st.session_state.selected_barangay} Dengue Forecast**")
         
         # Line Chart using st.line_chart
         if not filtered_data_line_chart.empty:
@@ -434,4 +423,5 @@ with button_container:
     
 button_css = float_css_helper(width="3rem", height="3rem", right="0.8rem", top="0.6rem", transition=0)
 button_container.float(button_css)
+
 
